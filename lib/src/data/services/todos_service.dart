5134f37f55c8243a67a3faf65a8fbd2f2196a405
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-24 16:13:28 +0700

    add get todos

--- lib/src/data/services/todos_service.dart
+++ lib/src/data/services/todos_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:skeleton_test/src/data/model/todos.dart';
 
 import 'remote/config/api.dart';
 
@@ -7,5 +8,25 @@ class TodosService {
   static final _singleton = TodosService._internal();
   factory TodosService() => _singleton;
 
-  final Dio _api = Api().dioClient;
+  final Dio _client = Api().dioClient;
+  static const path = '/todos';
+  Future<List<Todos>> getTodos({int from = 0, int? limit}) async {
+    var params = <String, dynamic>{};
+    params['_start'] = from;
+    if (limit != null) {
+      params['_limit'] = limit;
+    }
+    var res = await _client.get(path, queryParameters: params);
+    if (res.statusCode == 200) {
+      if (res.data != null) {
+        var result = (res.data as List);
+        return result
+            .map<Todos>(
+              (e) => Todos.fromJson(e),
+            )
+            .toList();
+      }
+    }
+    throw Exception('network error');
+  }
 }
