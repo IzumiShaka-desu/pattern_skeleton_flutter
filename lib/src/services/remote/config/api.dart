90179c8515f8e0128da074c444d282bde934fb7b
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-24 15:08:26 +0700

    update: create dio client

--- lib/src/services/remote/config/api.dart
+++ lib/src/services/remote/config/api.dart
@@ -1,5 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
+
 class Api {
   Api._internal();
+
   static final _singleton = Api._internal();
+
   factory Api() => _singleton;
+
+  final _dio = Dio(
+    BaseOptions(
+      baseUrl: dotenv.env["BASEURL"]!,
+      receiveTimeout: 3000,
+      sendTimeout: 5000,
+    ),
+  );
+
+  Dio get dioClient => _dio;
 }
