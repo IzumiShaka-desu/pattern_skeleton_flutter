84098910b04c9813d4b4de8632007d4c50ef14c1
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-24 13:37:50 +0700

    load dotenv variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -13,8 +14,15 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
+  ///
+  ///load env variabel
+  ///
+
+  await dotenv.load(fileName: ".env");
+
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
+
   runApp(MyApp(settingsController: settingsController));
 }
