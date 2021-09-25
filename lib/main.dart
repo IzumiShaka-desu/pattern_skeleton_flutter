d3c4220bb9191eaa7b4bd1ba6400d32cbd1a15a2
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-25 16:54:18 +0700

    restructure project dir

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+import 'src/modules/settings/settings_controller.dart';
+import 'src/modules/settings/settings_service.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
