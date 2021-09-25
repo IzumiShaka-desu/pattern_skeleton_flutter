dfcac08cff53d3a19fb4f9614363789e03d40622
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-25 07:42:43 +0700

    add scaffoldmessenger

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:skeleton_test/src/todos/todos_screen.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
   final SettingsController settingsController;
 
   static GlobalKey gkey = GlobalKey();
+  static GlobalKey<ScaffoldMessengerState> smkey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +31,7 @@ class MyApp extends StatelessWidget {
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
           key: gkey,
+          scaffoldMessengerKey: smkey,
           // Providing a restorationScopeId allows the Navigator built by the
           // MaterialApp to restore the navigation stack when a user leaves and
           // returns to the app after it has been killed while running in the
@@ -76,8 +79,10 @@ class MyApp extends StatelessWidget {
                   case SampleItemDetailsView.routeName:
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
+                  case TodosScreen.routeName:
+                    return const TodosScreen();
                   default:
-                    return const SampleItemListView();
+                    return const TodosScreen();
                 }
               },
             );
