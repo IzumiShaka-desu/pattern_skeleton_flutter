b22c960c9ceb9e88196bedcbc10ea1d6e4de039d
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-25 10:59:30 +0700

    restructure project dir

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:skeleton_test/src/todos/todos_screen.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
+import 'modules/sample_feature/sample_item_details_view.dart';
+import 'modules/sample_feature/sample_item_list_view.dart';
+import 'modules/settings/settings_controller.dart';
+import 'modules/settings/settings_view.dart';
+import 'modules/todos/todos_screen.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
